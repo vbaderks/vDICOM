04038eecf458081e42a774768d59ff31d39167a1
Victor Derks <vbaderks@gmail.com>
2014-02-20 00:17:09 +0100

    Added support for more VRs

--- src/representation.ts
+++ src/representation.ts
@@ -17,22 +17,38 @@ module Dicom {
     export class Representations {
         // Define all the Value Representations of table PS 3.5, 6.2-1
         public static applicationEntity = new Representation("AE", "Application Entity", false, false);
+        public static ageString = new Representation("AS", "Age String", false, false); // TODO: check attributes
+        public static attributeTag = new Representation("AT", "AttributeTag", false, false); // TODO: check attributes
+        public static codeString = new Representation("CS", "Code String", true, false); // TODO: check attributes
+        public static date = new Representation("DA", "Date", true, false); // TODO: check attributes
+        public static decimalString = new Representation("DS", "Date", true, false); // TODO: check attributes
+        public static dateTime = new Representation("DT", "Date Time", true, false); // TODO: check attributes
+        public static floatingPointSingle = new Representation("FL", "Floating Point Single", true, false); // TODO: check attributes
+        public static floatingPointDouble = new Representation("FD", "Floating Point Double", true, false); // TODO: check attributes
         public static integerString = new Representation("IS", "Integer String", true, false);
-        public static none = new Representation("NN", "None", false, false);
+        public static none = new Representation("", "None", false, false);
 
         private static createMap() {
             var map = {};
             map[Representations.applicationEntity.vr] = Representations.applicationEntity;
+            map[Representations.ageString.vr] = Representations.ageString;
+            map[Representations.attributeTag.vr] = Representations.attributeTag;
+            map[Representations.codeString.vr] = Representations.codeString;
+            map[Representations.date.vr] = Representations.date;
+            map[Representations.decimalString.vr] = Representations.decimalString;
+            map[Representations.dateTime.vr] = Representations.dateTime;
+            map[Representations.floatingPointSingle.vr] = Representations.floatingPointSingle;
+            map[Representations.floatingPointDouble.vr] = Representations.floatingPointDouble;
             map[Representations.integerString.vr] = Representations.integerString;
             return map;
         }
 
         private static lookupMap = Representations.createMap();
 
-        public static lookup(vr: string) {
+        public static lookup(vr: string): Representation {
             var representation = Representations.lookupMap[vr];
             if (representation == null) {
-                // TODO: handle error conditions.
+                throw new Error("Invalid data: detected unknown VR: " + vr);
             }
 
             return representation;
--- test/representations.spec.js
+++ test/representations.spec.js
@@ -1,15 +1,22 @@
 ﻿/// <reference path="../lib/jasmine.js"/>
 /// <reference path="../src/vdicom.js"/>
 
-describe("jsDICOM - Dicom.Representations", function () {
+describe("vDICOM - Dicom.Representations", function () {
     it("should be able to lookup all defined DICOM VRs", function () {
-        var representation = Dicom.Representations.lookup("IS");
-        expect(representation).toBe(Dicom.Representations.integerString);
         expect(Dicom.Representations.lookup("AE")).toBe(Dicom.Representations.applicationEntity);
+        expect(Dicom.Representations.lookup("AS")).toBe(Dicom.Representations.ageString);
+        expect(Dicom.Representations.lookup("AT")).toBe(Dicom.Representations.attributeTag);
+        expect(Dicom.Representations.lookup("CS")).toBe(Dicom.Representations.codeString);
+        expect(Dicom.Representations.lookup("DA")).toBe(Dicom.Representations.date);
+        expect(Dicom.Representations.lookup("DS")).toBe(Dicom.Representations.decimalString);
+        expect(Dicom.Representations.lookup("DT")).toBe(Dicom.Representations.dateTime);
+        expect(Dicom.Representations.lookup("FL")).toBe(Dicom.Representations.floatingPointSingle);
+        expect(Dicom.Representations.lookup("FD")).toBe(Dicom.Representations.floatingPointDouble);
+        expect(Dicom.Representations.lookup("IS")).toBe(Dicom.Representations.integerString);
     });
 
     it("should be able to handle an invalid VR", function () {
-        // TODO
+        expect(Dicom.Representations.lookup.bind(null, "QQ")).toThrow();
     });
 
     it("should be able to handle future VRs", function () {
